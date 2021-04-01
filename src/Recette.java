127cb736c7f7c0c0e772448c644c850ef27f69d9
Justine Malphettes <jmalphet@pcalb-mm0000.univ-jfc.fr>
2021-04-01 08:27:46 +0200

    Ajout de la sérialisation (incomplète)

--- src/Recette.java
+++ src/Recette.java
@@ -11,7 +11,74 @@ public class Recette implements Serializable{
 	int cout;
 	int nbpersonne;
 	
+	public Recette() {
+		this.etapes=new ArrayList();
+	}
 	
+	public String toString() {
+		return this.nom + " ("+this.difficulte + ")";
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public ArrayList<Etape> getEtapes() {
+		return etapes;
+	}
+
+
+
+	public void setEtapes(ArrayList<Etape> etapes) {
+		this.etapes = etapes;
+	}
+
+
+
+	public int getDifficulte() {
+		return difficulte;
+	}
+
+
+
+	public void setDifficulte(int difficulte) {
+		this.difficulte = difficulte;
+	}
+
+
+
+	public int getCout() {
+		return cout;
+	}
+
+
+
+	public void setCout(int cout) {
+		this.cout = cout;
+	}
+
+
+
+	public int getNbpersonne() {
+		return nbpersonne;
+	}
+
+
+
+	public void setNbpersonne(int nbpersonne) {
+		this.nbpersonne = nbpersonne;
+	}
+
+
+
 	public Recette(String n, int d, int c, int nb, ArrayList<Etape> l) {
 		this.nom=n;
 		this.difficulte=d;
@@ -26,10 +93,10 @@ public class Recette implements Serializable{
 	
 	public static void main(String[] args) {
 		ArrayList<Etape> etape = new ArrayList<Etape>();
-		Ingredients bouillondelegumes=new Ingredients("bouillon de légumes");
+		Ingredients bouillondelegumes=new Ingredients("bouillon de lï¿½gumes");
 		ArrayList<Ingredients> ingredients=new ArrayList<Ingredients>();
-		ingredients.add(new Ingredients("bouillon de légumes"));
-		etape.add(new Etape ("Verser le bouillon de légumes",ingredients));
+		ingredients.add(new Ingredients("bouillon de lï¿½gumes"));
+		etape.add(new Etape ("Verser le bouillon de lï¿½gumes",ingredients));
 		Recette risotto=new Recette("risotto",2,5,4,etape);
 		System.out.println(risotto);
 	}
