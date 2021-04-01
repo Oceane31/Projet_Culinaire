73a8a444e3e0fef0f7dfd3a5ceaf35cdf016bbd2
Oceane31 <78899865+Oceane31@users.noreply.github.com>
2021-04-01 09:42:28 +0200

    Ajout des fenêtres

---
+++ src/GUI.java
@@ -0,0 +1,62 @@
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class GUI extends Frame implements WindowListener{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GUI();
+	}
+
+	public GUI() {
+		this.setLayout(new BorderLayout());
+		this.addWindowListener(this);
+		this.setTitle("application");
+		this.pack();
+		this.setVisible(true);
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		 
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/Recette.java
+++ src/Recette.java
@@ -37,3 +37,4 @@ public class Recette implements Serializable{
 	
 
 }
+
