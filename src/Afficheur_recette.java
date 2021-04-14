5753a142ee26815f7300910e50d00cf6b688b200
Eve <evedauba@gmail.com>
2021-04-14 18:17:30 +0200

    ajout boutons

--- src/Afficheur_recette.java
+++ src/Afficheur_recette.java
@@ -63,26 +63,36 @@ public class Afficheur_recette extends Panel implements Observer, ActionListener
 		
 		JButton paella= new JButton("Paella");
 		p.add(paella);
+		
+		JButton riz_senegalais = new JButton("Riz s�n�galais");
+		p.add(riz_senegalais);
 		onglet2.add(p, BorderLayout.NORTH);
 
 		Panel p1=new Panel();
 		JButton riz_au_lait= new JButton("Riz au lait");
 		p1.add(riz_au_lait);
-	
 		
-		JButton riz_sucré_au_four= new JButton("Riz sucré au four");
-		p1.add(riz_sucré_au_four);
-	
+		JButton riz_sucre_au_four= new JButton("Riz sucre au four");
+		p1.add(riz_sucre_au_four);
 		
-		JButton gateau_caramel= new JButton("Gâteau de riz au caramel");
+		JButton gateau_caramel= new JButton("Gateau de riz au caramel");
 		p1.add(gateau_caramel);
+		
+		JButton galette_de_riz = new JButton("Galette de riz");
+		p1.add(galette_de_riz);
 		onglet3.add(p1, BorderLayout.NORTH);
 		
 		
 		Panel p2=new Panel();
 		JButton salade_fruits_secs= new JButton("Salade de riz"+newLine+ " aux fruits secs");
-		//le newLine est censé être un retour à la ligne mais ça marche pas 
+		//le newLine est cense etre un retour a� la ligne mais ca marche pas 
 		p2.add(salade_fruits_secs);
+		
+		JButton salade_thon = new JButton("Salade de riz au thon");
+		p2.add(salade_thon);
+		
+		JButton avocat_salade = new JButton("Avocat au riz en salade");
+		p2.add(avocat_salade);
 		onglet1.add(p2, BorderLayout.NORTH);
 		
 		
